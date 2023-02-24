18e5030fbf0285bfc645c90dd73ff782d062b1e3
Nicolas Agustin Marco <nicolasmarco841@gmail.com>
2023-02-24 19:42:49 -0300

    Creada estructura Node.js + Express. Creados los gets correspondientes a las paginas, comentados los que todavia no se utilizan

---
+++ src/app.js
@@ -0,0 +1,52 @@
+const express = require ("express");
+const path = require ("path");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/" , (req , res) => {
+    
+    const rutaCompleta = path.join(__dirname , "/views/index.html");
+    res.sendFile(rutaCompleta);
+
+});
+
+/*
+
+app.get("/productDetail" , (req , res) => {
+    
+    const rutaCompleta = path.join(__dirname , "/views/productDetail.html");
+    res.sendFile(rutaCompleta);
+
+});
+
+
+app.get("/productCart" , (req , res) => {
+    
+    const rutaCompleta = path.join(__dirname , "/views/productCart.html");
+    res.sendFile(rutaCompleta);
+
+});
+
+app.get("/register" , (req , res) => {
+    
+    const rutaCompleta = path.join(__dirname , "/views/register.html");
+    res.sendFile(rutaCompleta);
+
+});
+
+app.get("/login" , (req , res) => {
+    
+    const rutaCompleta = path.join(__dirname , "/views/login.html");
+    res.sendFile(rutaCompleta);
+
+});
+
+*/
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log("Servidor funcionando en el puerto " + port) ;
+});
