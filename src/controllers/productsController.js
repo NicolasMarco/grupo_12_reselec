1b8f84c2c3fcc38cd17a5248699680acd5ef8765
Mariano Marco <mariano268@gmail.com>
2023-04-03 03:36:35 -0300

    Vista Editar productos trae los parámetros guardados salvo las imágenes. Falta actualizar la BBDD al guardar los cambios

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -10,7 +10,12 @@ const productsController = {
     },
 
     editarProducto: (req,res) => {
-        res.render("products/editarProducto");
+        const productos = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+        let id = req.params.id;
+        const productToSend = productos.find (product => {
+            return product.id == id
+        })
+        res.render("products/editarProducto", {producto: productToSend});
     },
 
     productCart: (req,res) => {
@@ -57,7 +62,38 @@ const productsController = {
 
 		res.redirect("/products");
 
+    },
+
+    patchProducto: function(req,res) {
+        const productos = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+        let productoACargar =  {
+            id: productos.id,
+            nombre: req.body.nombre,
+            precio: parseInt(req.body.precio),
+            categoria: req.body.categoria,
+            tipoEquipo: req.body.tipoEquipo,
+            caracteristicas : [req.body.caracteristicaUno , req.body.caracteristicaDos , req.body.caracteristicaTres],
+            descripcion: req.body.descripcion,
+            imagen: req.files[0].filename,
+            imagen2: req.files[1] ? req.files[1].filename : req.files[0].filename,
+            imagen3: req.files[2] ? req.files[2].filename : req.files[0].filename,
+            imagen4: req.files[3] ? req.files[3].filename : req.files[0].filename,
+            imagen5: req.files[4] ? req.files[4].filename : req.files[0].filename
+        };
+
+        productos.push(productoACargar);
+
+		let productosJSON = JSON.stringify(productos, null, " ");
+
+		fs.writeFileSync(productsFilePath , productosJSON);
+        
+
+		res.redirect("/products");
+
     }
+
+    
 };
 
 module.exports = productsController;
