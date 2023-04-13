6d294c8a006529c4ff3acb045b2a51ec248f64a8
Nicolas Agustin Marco <nicolasmarco841@gmail.com>
2023-04-12 23:39:14 -0300

    Imagenes organizadas. Estructura JSON de usuarios modificada. Vista de registro modificada. Registro basico completo, resta configurar las validaciones correspondientes

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,10 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+
+const usersFilePath = path.join(__dirname, '../data/usersDataBase.json');
+
 const usersController = {
     login: (req,res) => {
         res.render("users/login");
     },
 
-    register: (req,res) => {
+    getRegister: (req,res) => {
         res.render("users/register");
+    },
+
+    userRegister: (req,res) => {
+        const usuarios = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+
+        let usuarioACargar =  {
+            id: (usuarios[usuarios.length-1].id + 1),
+            usuario: req.body.usuario,
+            contraseña: req.body.contraseña,
+            nombre: req.body.nombre,
+            apellido: req.body.apellido,
+            email: req.body.email,
+            telefono: req.body.telefono,
+            imagen: req.file ? req.file.filename : "default.png",
+            administrador: true
+        };
+
+        usuarios.push(usuarioACargar);
+
+		let usuariosJSON = JSON.stringify(usuarios, null, " ");
+
+		fs.writeFileSync(usersFilePath , usuariosJSON);
+        
+		res.redirect("/users/login");
     }
 };
 
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,10 +1,23 @@
 const express = require ("express");
 const router = express.Router();
+const multer = require ("multer");
+const path = require("path");
+
+let storage = multer.diskStorage({
+    destination: function(req , file , cb) {
+        cb(null , "public/images/users");
+    },
+    filename: function(req , file , cb) {
+        cb(null , file.fieldname + "-" + Date.now() + path.extname(file.originalname) );
+    }
+});
 
 const usersController = require ("../controllers/usersController.js");
+const upload = multer({storage : storage});
 
-//CargarProducto
 router.get("/login" , usersController.login);
-router.get("/register" , usersController.register);
+router.get("/register" ,  usersController.getRegister);
+
+router.post("/login" ,upload.single("imagenUsuario"), usersController.userRegister);
 
 module.exports = router;
